095ae667b1b9e54f415b11ccb9541b46d889a36d
Johannes <johannes@axner.io>
2022-04-07 21:39:39 +0200

    refactor(client): some temporary error handling for api call

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -4,23 +4,35 @@ import { GetServerSideProps, NextPage } from 'next';
 import { EventCardList, Page } from '@larpcalendar/components';
 export interface IndexProps {
   events: Array<LarpEvent>;
+  error: any;
 }
 
-export const Index: NextPage<IndexProps> = ({ events = [] }) => (
-  <Page title="Events" crumbs={[{ label: 'Events', href: '/' }]}>
-    <p>NODE_ENV: {process?.env?.NODE_ENV || 'No env'}</p>
-    <p>NEXT_PUBLIC_API_URL: {process?.env?.NEXT_PUBLIC_API_URL || 'No api'}</p>
-    <EventCardList events={events} />
-  </Page>
-);
+export const Index: NextPage<IndexProps> = ({ events = [], error }) => {
+  console.log({ events, error });
+  return (
+    <Page title="Events" crumbs={[{ label: 'Events', href: '/' }]}>
+      <p>NODE_ENV: {process?.env?.NODE_ENV || 'No env'}</p>
+      <p>
+        NEXT_PUBLIC_API_URL: {process?.env?.NEXT_PUBLIC_API_URL || 'No api'}
+      </p>
+      <EventCardList events={events} />
+    </Page>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps<IndexProps> = async (
   _context
 ) => {
-  // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
-  // const events = response ? await response.json() : [];
-  const events = [];
-  return { props: { events } };
+  let error;
+  let events = [];
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
+    events = response ? await response.json() : [];
+  } catch (err) {
+    error = err;
+  }
+  console.log({ events, error });
+  return { props: { events, error } };
 };
 
 export default Index;

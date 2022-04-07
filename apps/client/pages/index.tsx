a5e803f394d816c415ad1428fefb7a9acea80cc8
Johannes <johannes@axner.io>
2022-04-07 21:49:44 +0200

    refactor(client): removed debug code

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -4,17 +4,11 @@ import { GetServerSideProps, NextPage } from 'next';
 import { EventCardList, Page } from '@larpcalendar/components';
 export interface IndexProps {
   events: Array<LarpEvent>;
-  error: any;
 }
 
-export const Index: NextPage<IndexProps> = ({ events = [], error }) => {
-  console.log({ events, error });
+export const Index: NextPage<IndexProps> = ({ events = [] }) => {
   return (
     <Page title="Events" crumbs={[{ label: 'Events', href: '/' }]}>
-      <p>NODE_ENV: {process?.env?.NODE_ENV || 'No env'}</p>
-      <p>
-        NEXT_PUBLIC_API_URL: {process?.env?.NEXT_PUBLIC_API_URL || 'No api'}
-      </p>
       <EventCardList events={events} />
     </Page>
   );
@@ -23,16 +17,14 @@ export const Index: NextPage<IndexProps> = ({ events = [], error }) => {
 export const getServerSideProps: GetServerSideProps<IndexProps> = async (
   _context
 ) => {
-  let error;
   let events = [];
   try {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
     events = response ? await response.json() : [];
-  } catch (err) {
-    error = err;
+  } catch (error) {
+    console.error(error);
   }
-  console.log({ events, error });
-  return { props: { events, error } };
+  return { props: { events } };
 };
 
 export default Index;
